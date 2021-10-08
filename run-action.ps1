564a34b20e4caa1929432a73a2200a6bbe466dc2
Simon Timms <stimms@gmail.com>
2021-10-08 12:16:19 -0600

    Initial commit

---
+++ run-action.ps1
@@ -0,0 +1,23 @@
+if($IsWindows -eq $True)
+{
+    $filePath = Join-Path $PSScriptRoot "/dist/win-x64/dotnet-sample-action.exe"
+}
+
+if($IsLinux -eq $True)
+{
+    $filePath = Join-Path $PSScriptRoot "/dist/linux-x64/dotnet-sample-action"
+    chmod +x $filePath
+}
+
+if($IsMacOS -eq $True)
+{
+    $filePath = Join-Path $PSScriptRoot "/dist/osx-x64/dotnet-sample-action"
+    chmod +x $filePath
+}
+
+Write-Output "Running $filePath..."
+& $filePath
+
+if ($LASTEXITCODE -ne 0) {
+    exit $LASTEXITCODE
+}
---
+++ src/Program.cs
@@ -0,0 +1,31 @@
+﻿using DotnetActionsToolkit;
+using System;
+using System.Collections;
+using System.Threading.Tasks;
+
+namespace dotnet_sample_action
+{
+    public class Program
+    {
+        static readonly Core _core = new Core();
+
+        static async Task Main(string[] args)
+        {
+            try
+            {
+                 var ms = _core.GetInput("milliseconds");
+                 _core.Debug($"Waiting {ms} milliseconds..."); // debug is only output if you set teh secret ACTIONS_RUNNER_DEBUG to true
+
+                 _core.Debug(DateTime.Now.ToLongTimeString());
+                 await Task.Delay(int.Parse(ms));
+                 _core.Debug(DateTime.Now.ToLongTimeString());
+
+                 _core.SetOutput("time", DateTime.Now.ToLongTimeString());
+            }
+            catch (Exception ex)
+            {
+                _core.SetFailed(ex.Message);
+            }
+        }
+    }
+}
